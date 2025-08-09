0e773ffb660ecd112a3a4414fab9135fed4951da
CaioLira18 <caiozao1212@gmail.com>
2025-08-09 01:04:46 -0300

    [feat] - update

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,18 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
-import './css/Home.css'
-import './css/Footer.css'
+import './css/Franquia.css'
 import './css/Responsividade.css'
 import './css/Slide.css'
-import './css/Franquia.css'
+import './css/Register.css'
+import './css/Edit.css'
+import './css/Footer.css'
+import './css/AdicionarTemporadas.css'
 import './css/Login.css'
 import './css/Movie.css'
-import './css/Serie.css'
 import './css/Header.css'
-import './css/PageAll.css'
+import './css/Serie.css'
+import './css/Home.css'
+
 
 import App from './App.jsx'
+
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
