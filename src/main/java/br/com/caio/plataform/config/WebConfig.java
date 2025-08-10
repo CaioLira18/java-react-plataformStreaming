e6f845a20340eb920497e06d236253b6295da837
CaioLira18 <caiozao1212@gmail.com>
2025-08-09 21:21:06 -0300

    [feat] - update

--- src/main/java/br/com/caio/plataform/config/WebConfig.java
+++ src/main/java/br/com/caio/plataform/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://java-react-plataform-streaming.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
                 // Remove .allowCredentials(true)
